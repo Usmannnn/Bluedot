5ae747e986e818d7cda4379a4dcb6f78c7b71311
MID2015 <mid2015@MID2015s-MacBook-Pro.local>
2022-03-29 22:41:27 +0300

    total distance

--- src/screens/Tracking.js
+++ src/screens/Tracking.js
@@ -5,8 +5,6 @@ import FreeDriveBar from '../components/FreeDriveBar';
 import MyLocation from '../components/MyLocation';
 import * as Location from 'expo-location';
 
-
-
 const Tracking = () => {
 
 	const mapRef = useRef();
@@ -21,6 +19,7 @@ const Tracking = () => {
 
 	const getCurrentPosition = async () => {
 		const { coords } = await Location.getCurrentPositionAsync({})
+		console.log(coords)
 		setCurrentPosition({ latitude: coords.latitude, longitude: coords.longitude })
 		mapRef.current.animateToRegion({
 			latitude: coords.latitude,
@@ -30,14 +29,47 @@ const Tracking = () => {
 		});
 	}
 
-	useEffect(() => { getCurrentPosition() }, [])
+	useEffect(() => {
+		getCurrentPosition()
+		// fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.8538996,29.3002546&radius=1000&type=point_of_interest&keyword=divvy&key=AIzaSyA8Fc0HE-Vphp5UVgmFlPa0Od_-w8H2YJI`)
+		// 	.then(res => res.json())
+		// 	.then(res => console.log(res))
+	}, [])
 
 	const toggleFreeDriving = () => {
 		if (drivingCoordinates.length > 0) setDrivingCoordinates([])
 		else setDrivingCoordinates([currentPosition])
 	}
 
-	useEffect(() => { console.log(drivingCoordinates.length) }, [drivingCoordinates])
+	useEffect(() => {
+		console.log(drivingCoordinates.length)
+		calculateDistance(drivingCoordinates)
+	}, [drivingCoordinates])
+
+
+	const calculateDistance = (arr) => {
+
+		let totalDistance = 0
+		for (let i = 1; i < arr.length; i++) {
+			totalDistance += haversine(arr[i - 1], arr[i])
+		}
+		console.log(totalDistance)
+	}
+
+	function haversine(coords1, coords2) {
+		const R = 6371e3; // metres
+		const φ1 = coords1.latitude * Math.PI / 180; // φ, λ in radians
+		const φ2 = coords2.latitude * Math.PI / 180;
+		const Δφ = (coords2.latitude - coords1.latitude) * Math.PI / 180;
+		const Δλ = (coords2.longitude - coords1.longitude) * Math.PI / 180;
+
+		const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+			Math.cos(φ1) * Math.cos(φ2) *
+			Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+		const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+		return R * c; // in metres
+	}
 
 
 	return (
